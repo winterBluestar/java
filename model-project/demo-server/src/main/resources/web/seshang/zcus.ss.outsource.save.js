db475950011bd0a1ebb5ece26465bb39cdae4d4a
chenxudong <xudong.chen@zone-cloud.com>
2022-11-08 16:52:25 +0800

    [add]jf20221108委外出入库保存/保存并提交,批量提交

--- model-project/demo-server/src/main/resources/web/seshang/zcus.ss.outsource.save.js
+++ model-project/demo-server/src/main/resources/web/seshang/zcus.ss.outsource.save.js
@@ -6,30 +6,6 @@ function process(input) {
     const locatorModeler = 'zpfm_locator';
     const storageModeler = 'zcus_ss_outsource_storage';
     const storageLineModeler = 'zcus_ss_outsource_storage_line';
-    // 校验头仓是否为菜鸟仓且整张单据仓库是否一致
-    const birdWarehouseMap = new Map();
-    if (input.outsourceLineList != null && input.outsourceLineList.length > 0) {
-        input.outsourceLineList.forEach(function (value, index) {
-            birdWarehouseMap.set(value.warehouseId, value.syncFlag)
-        })
-    }
-    if (!isEmptyStr(input.warehouseId) && isEmptyStr(input.syncFlag)) {
-        if (input.outsourceLineList != null && input.outsourceLineList.length > 0) {
-            input.outsourceLineList.forEach(function (value, index) {
-                if (!input.warehouseId === value.warehouseId) {
-                    H0.ExceptionHelper.throwCommonException("头行仓库需保持一致！");
-                }
-            })
-        }
-
-    }
-    if (isEmptyStr(input.warehouseId) && birdWarehouseMap != null && birdWarehouseMap.size > 0) {
-        birdWarehouseMap.forEach(function (value, index) {
-            if (value.syncFlag === 'true') {
-                H0.ExceptionHelper.throwCommonException("头行仓库需保持一致！");
-            }
-        })
-    }
     // 校验单据类型为“委外出库”时库存量是否大于用户填写的数量
     if (input.docTypeCode === 'OUT') {
         if (input.outsourceLineList != null && input.outsourceLineList.length > 0) {
@@ -40,7 +16,7 @@ function process(input) {
                     "itemId": input.itemId,
                     "itemSkuId": input.itemSkuId
                 });
-                if (stockRes != null || stockRes != "{}") {
+                if (stockRes != null || stockRes !== "{}") {
                     if (value.quantity > stockRes.quantity) {
                         H0.ExceptionHelper.throwCommonException(value.lineNumber + "当前仓库库存数量小于执行数量！请确认后重新提交！");
                     }
@@ -52,16 +28,13 @@ function process(input) {
     if (input.isSubmit === 'true') {
         // 推送旺店通
         if (!isEmptyStr(input.syncFlag) && input.syncFlag === 'true') {
-            if (input.outsourceLineList != null && input.outsourceLineList.length > 0) {
-                input.outsourceLineList.forEach(function (value, index) {
-                    value.syncStatus = 'PUSHED'
-                })
-                //TODO 将出入库指令推送给外部系统旺店通
-            }
+            input.docStatusCode = 'PUSHED'
+            H0.ModelerHelper.updateByPrimaryKey(storageModeler, tenantId, storage, true)
+            //TODO 将出入库指令推送给外部系统旺店通
         }
         // 不推送旺店通
         if (isEmptyStr(input.syncFlag) || (!isEmptyStr(input.syncFlag) && input.syncFlag === 'false')) {
-            input.syncStatus = 'COMPLETED';
+            input.docStatusCode = 'COMPLETED';
             if (input.outsourceLineList != null && input.outsourceLineList.length > 0) {
                 input.outsourceLineList.forEach(function (value, index) {
                     value.executeQty = value.quantity
@@ -89,7 +62,7 @@ function process(input) {
             }
             // 拼接杂入杂出的路径
             if (input.docTypeCode === 'IN') {
-                invokePath = "/v1/" + tenantId + "/rest/invoke";
+                invokePath = "/hitf/v1/" + tenantId + "/rest/invoke";
             } else if (input.docTypeCode === 'OUT') {
                 invokePath = "/hitf/v1" + tenantId + "/rest/invoke?namespace=HZERO&serverCode=ZOSC_OPEN_API&interfaceCode=zosc-open-api.stock-api-controller.miscOut"
             }
---
+++ model-project/demo-server/src/main/resources/web/seshang/zcus.ss.outsource.submit.js
@@ -0,0 +1,66 @@
+function process(input) {
+    BASE.Logger.debug('-------input-------{}', input)
+    //const tenantId = CORE.CurrentContext.getTenantId();
+    const tenantId = 76;
+    const locatorModeler = 'zpfm_locator';
+    const storageModeler = 'zcus_ss_outsource_storage';
+    const storageLineModeler = 'zcus_ss_outsource_storage_line';
+
+    // 根据id查询委外出入库头数据
+    if (input != null && input.docList != null && input.docList.length > 0) {
+        input.docList.forEach(function (value, key) {
+            const storage = H0.ModelerHelper.selectOne(storageModeler, tenantId, {
+                "docId": value.docId
+            });
+            if (storage == null) {
+                H0.ExceptionHelper.throwCommonException("委外出入库订单" + value.docNum + "不存在，请确认后重试！");
+            }
+            if (storage.docStatusCode !== 'NEW') {
+                H0.ExceptionHelper.throwCommonException("委外出入库订单" + value.docNum + "状态不为新建，请确认后重试！");
+            }
+            if (storage.syncFlag === '1') {
+                //todo 将订单状态更新为 已推送, 并将出入库指令推送给旺店通
+                storage.docStatusCode = 'PUSHED'
+            } else if (storage.syncFlag === '0') {
+                // 更新状态为已完成, 更新行执行数量 = 数量, 调用杂入杂出接口
+                storage.docStatusCode = 'COMPLETED'
+                const storageLineList = H0.ModelerHelper.selectList(storageLineModeler, tenantId, {
+                    "docId": value.docId
+                });
+                if (storageLineList == null || storageLineList.length <= 0) {
+                    H0.ExceptionHelper.throwCommonException("委外出入库订单" + value.docNum + "不存在行数据，请确认后重试！");
+                }
+                const paramList = [];
+                storageLineList.forEach(function (value, index) {
+                    value.executeQty = value.quantity
+                    const locator = H0.ModelerHelper.selectOne(locatorModeler, tenantId, {
+                        "locatorId": value.locatorId
+                    });
+                    const param = {
+                        organizationCode: input.organizationCode,
+                        warehouseCode: value.warehouseCode,
+                        locatorCode: locator.locatorCode,
+                        itemCode: value.itemCode,
+                        itemSkuCode: value.itemSkuCode,
+                        uomName: value.uomName,
+                        miscInQty: value.quantity,
+                        executeTime: new Date()
+                    }
+                    paramList.push(param)
+                })
+                // 更新行执行数量
+                H0.ModelerHelper.batchUpdateByPrimaryKey(storageLineModeler, tenantId, storageLineList, true)
+                // 拼接杂入杂出的路径
+                let invokePath;
+                if (input.docTypeCode === 'IN') {
+                    invokePath = "/hitf/v1/" + tenantId + "/rest/invoke";
+                } else if (input.docTypeCode === 'OUT') {
+                    invokePath = "/hitf/v1" + tenantId + "/rest/invoke?namespace=HZERO&serverCode=ZOSC_OPEN_API&interfaceCode=zosc-open-api.stock-api-controller.miscOut"
+                }
+                H0.FeignClient.selectClient('zosc-open-api').doPost(invokePath, paramList)
+            }
+            H0.ModelerHelper.updateByPrimaryKey(storageModeler, tenantId, storage, true)
+        })
+    }
+    return input;
+}
