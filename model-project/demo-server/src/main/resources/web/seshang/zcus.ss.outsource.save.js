86697646ead9b86f8ecf8d95d269963c6dbfad63
chenxudong <xudong.chen@zone-cloud.com>
2022-11-10 13:25:22 +0800

    [add]

--- model-project/demo-server/src/main/resources/web/seshang/zcus.ss.outsource.save.js
+++ model-project/demo-server/src/main/resources/web/seshang/zcus.ss.outsource.save.js
@@ -3,9 +3,9 @@ function process(input) {
     const tenantId = CORE.CurrentContext.getTenantId();
     //const tenantId = 76;
     const stockModeler = 'zinv_stock';
-    const locatorModeler = 'zpfm_locator';
     const storageModeler = 'zcus_ss_outsource_storage';
     const storageLineModeler = 'zcus_ss_outsource_storage_line';
+    const scriptCode = 'zcus.ss.outsource.submit';
     // 校验单据类型为“委外出库”时库存量是否大于用户填写的数量
     if (input.docTypeCode === 'OUT') {
         if (input.outsourceLineList != null && input.outsourceLineList.length > 0) {
@@ -24,52 +24,6 @@ function process(input) {
             })
         }
     }
-    // 判断是否为保存并提交
-    if (input.isSubmit === 'true') {
-        // 推送旺店通
-        if (!isEmptyStr(input.invType) && input.invType !== 'SCC') {
-            input.docStatusCode = 'PUSHED'
-            H0.ModelerHelper.updateByPrimaryKey(storageModeler, tenantId, storage, true)
-            //TODO 将出入库指令推送给外部系统旺店通
-        }
-        // 不推送旺店通
-        if (isEmptyStr(input.invType) || (!isEmptyStr(input.invType) && input.invType === 'SCC')) {
-            input.docStatusCode = 'COMPLETED';
-            if (input.outsourceLineList != null && input.outsourceLineList.length > 0) {
-                input.outsourceLineList.forEach(function (value, index) {
-                    value.executeQty = value.quantity
-                })
-            }
-            const paramList = [];
-            let invokePath;
-            if (input.outsourceLineList != null && input.outsourceLineList.length > 0) {
-                input.outsourceLineList.forEach(function (value, index) {
-                    const locator = H0.ModelerHelper.selectOne(locatorModeler, tenantId, {
-                        "locatorId": value.locatorId
-                    });
-                    const param = {
-                        organizationCode: input.organizationCode,
-                        warehouseCode: value.warehouseCode,
-                        locatorCode: locator.locatorCode,
-                        itemCode: value.itemCode,
-                        itemSkuCode: value.itemSkuCode,
-                        uomName: value.uomName,
-                        miscInQty: value.quantity,
-                        executeTime: new Date()
-                    }
-                    paramList.push(param)
-                })
-            }
-            // 拼接杂入杂出的路径
-            if (input.docTypeCode === 'IN') {
-                invokePath = "/hitf/v1/" + tenantId + "/rest/invoke";
-            } else if (input.docTypeCode === 'OUT') {
-                invokePath = "/hitf/v1" + tenantId + "/rest/invoke?namespace=HZERO&serverCode=ZOSC_OPEN_API&interfaceCode=zosc-open-api.stock-api-controller.miscOut"
-            }
-            H0.FeignClient.selectClient('zosc-open-api').doPost(invokePath, paramList)
-        }
-    }
-
     // 保存头数据
     let resHead;
     if (input._status === 'create') {
@@ -90,6 +44,17 @@ function process(input) {
                 H0.ModelerHelper.deleteByPrimaryKey(storageLineModeler, tenantId, value)
             }
         })
+    }
+    // 判断是否为保存并提交, 如果是的话则调用提交脚本
+    if (input.isSubmit === 'true') {
+        const docList = [];
+        const param = {
+            docId: resHead.docId,
+            docNum: resHead.docNum,
+            organizationCode: input.organizationCode
+        }
+        docList.push(param)
+        H0.ScriptHelper.execute (tenantId, scriptCode, docList);
     }
     return input;
 }
