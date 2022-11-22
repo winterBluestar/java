285b2886d20de456ad92d03c9435714ddf356b2b
chenxudong <xudong.chen@zone-cloud.com>
2022-11-22 10:56:40 +0800

    [fix]

--- model-project/demo-server/src/main/resources/web/seshang/zcus.ss.outsource.popularize.sale .js
+++ model-project/demo-server/src/main/resources/web/seshang/zcus.ss.outsource.popularize.sale .js
@@ -11,8 +11,10 @@ function process(input) {
     const locatorModeler = 'zpfm_locator';
     const codeValueMap = new Map();
     const buildParamsPath = "/v1/" + tenantId + "/ss-wdt/bulid-wdt-params";
-    const createPoPath = "/v2/rest/invoke?namespace=" + CORE.CurrentContext.getTenantNum() + "&serverCode=ZCUS.SS.WDT_INTERFACE&interfaceCode=purchaseOrderPush";
-    const createPoStockInPath = "/v2/rest/invoke?namespace=" + CORE.CurrentContext.getTenantNum() + "&serverCode=ZCUS.SS.WDT_INTERFACE&interfaceCode=stockinPurchasePush";
+    const createPoPath = "/v1/" + tenantId + "/ss-wdt/wdt-interface-invoke/PURCHASE_ORDER_PUSH";
+    const createPoStockInPath = "/v1/" + tenantId + "/ss-wdt/wdt-interface-invoke/STOCKIN_PURCHASE_PUSH";
+    //const createPoPath = "/v2/rest/invoke?namespace=" + CORE.CurrentContext.getTenantNum() + "&serverCode=ZCUS.SS.WDT_INTERFACE&interfaceCode=purchaseOrderPush";
+    //const createPoStockInPath = "/v2/rest/invoke?namespace=" + CORE.CurrentContext.getTenantNum() + "&serverCode=ZCUS.SS.WDT_INTERFACE&interfaceCode=stockinPurchasePush";
     const queryPoStockInPath = "/v2/rest/invoke?namespace=" + CORE.CurrentContext.getTenantNum() + "&serverCode=ZCUS.SS.WDT_INTERFACE&interfaceCode=stockinOrderQueryPurchase";
     // 创建采购单
     let storage = H0.ModelerHelper.selectOne(storageModeler, tenantId, {
@@ -50,48 +52,49 @@ function process(input) {
             }
         }
         poHead.details_list = detailsList
-        const createPoParam = {
-
-        }
+        const createPoParam = {}
         createPoParam.purchase_info = CORE.JSON.stringify(poHead)
+        createPoParam.keyCode = storage.docNum
         // 调用zosc-second-service获取参数
         BASE.Logger.debug('-------createPoParam-------{}', createPoParam)
-        let createPoParamRes = BASE.FeignClient.selectClient(secondServiceId)
-            .doPost(buildParamsPath, createPoParam);
-        BASE.Logger.debug('-------createPoParamRes-------{}', createPoParamRes)
-        createPoParamRes = CORE.JSON.parse(createPoParamRes);
-        if (createPoParamRes.sign != null) {
-            const param = {
-                bodyParamMap: createPoParamRes
-            }
-            // 调用旺店通创建采购单接口
-            BASE.Logger.debug('-------调用旺店通创建采购订单接口参数-------{}', param)
-            let createWdtRes = BASE.FeignClient.selectClient(serverId)
-                .doPost(createPoPath, param);
-            createWdtRes = CORE.JSON.parse(createWdtRes);
-            BASE.Logger.debug("-------CREATE WDT RES-------{}", createWdtRes)
-            const payloadRes = CORE.JSON.parse(createWdtRes.payload);
-            if (payloadRes.code == 0) {
-                // 更新创建采购单状态
+        //let createPoParamRes = BASE.FeignClient.selectClient(secondServiceId)
+        //    .doPost(buildParamsPath, createPoParam);
+        //BASE.Logger.debug('-------createPoParamRes-------{}', createPoParamRes)
+        //createPoParamRes = CORE.JSON.parse(createPoParamRes);
+        //if (createPoParamRes.sign != null) {
+        //    const param = {
+        //        bodyParamMap: createPoParamRes
+        //    }
+        // 调用旺店通创建采购单接口
+        //BASE.Logger.debug('-------调用旺店通创建采购订单接口参数-------{}', createPoParam)
+        //let createWdtRes = BASE.FeignClient.selectClient(serverId)
+        //    .doPost(createPoPath, param);
+        let createWdtRes = BASE.FeignClient.selectClient(secondServiceId)
+            .doPost(createPoPath, createPoParam);
+        createWdtRes = CORE.JSON.parse(createWdtRes);
+        BASE.Logger.debug("-------CREATE PO RES-------{}", createWdtRes)
+        const payloadRes = CORE.JSON.parse(createWdtRes.message);
+        if (payloadRes.code == 0) {
+            // 更新创建采购单状态
+            storage.wdtDocNum = storage.docNum
+            storage.syncStatus = null
+            storage.syncMsg = null
+            H0.ModelerHelper.updateByPrimaryKey(storageModeler, tenantId, storage, true)
+        } else {
+            if (payloadRes.code == 7010) {
+                // 已经存在采购入库单, 采购单号为外部单号, 更新采购单状态
                 storage.wdtDocNum = storage.docNum
                 storage.syncStatus = null
                 storage.syncMsg = null
                 H0.ModelerHelper.updateByPrimaryKey(storageModeler, tenantId, storage, true)
             } else {
-                if (payloadRes.code == 7010) {
-                    // 已经存在采购入库单, 采购单号为外部单号, 更新采购单状态
-                    storage.wdtDocNum = storage.docNum
-                    storage.syncStatus = null
-                    storage.syncMsg = null
-                    H0.ModelerHelper.updateByPrimaryKey(storageModeler, tenantId, storage, true)
-                } else {
-                    storage.syncStatus = '失败'
-                    storage.syncMsg = payloadRes.message
-                    H0.ModelerHelper.updateByPrimaryKey(storageModeler, tenantId, storage, true)
-                    return payloadRes.message
-                }
+                storage.syncStatus = '失败'
+                storage.syncMsg = payloadRes.message
+                H0.ModelerHelper.updateByPrimaryKey(storageModeler, tenantId, storage, true)
+                return payloadRes.message
             }
         }
+        //}
     }
     // 创建采购入库单
     storage = H0.ModelerHelper.selectOne(storageModeler, tenantId, {
@@ -130,69 +133,70 @@ function process(input) {
             }
         }
         purchaseInfo.details_list = detailsList
-        const createStockInParam = {
-
-        }
+        const createStockInParam = {}
         createStockInParam.purchase_info = CORE.JSON.stringify(purchaseInfo)
+        createStockInParam.keyCode = storage.docNum
         // 调用zosc-second-service获取参数
         BASE.Logger.debug('-------createStockInParam-------{}', createStockInParam)
-        let createStockInParamRes = BASE.FeignClient.selectClient(secondServiceId)
-            .doPost(buildParamsPath, createStockInParam);
-        BASE.Logger.debug('-------createStockInParamRes-------{}', createStockInParamRes)
-        createStockInParamRes = CORE.JSON.parse(createStockInParamRes)
-        if (createStockInParamRes.sign != null) {
-            const param = {
-                bodyParamMap: createStockInParamRes
-            }
-            // 调用旺店通创建采购入库单接口
-            let createWdtStockInRes = BASE.FeignClient.selectClient(serverId)
-                .doPost(createPoStockInPath, param);
-            createWdtStockInRes = CORE.JSON.parse(createWdtStockInRes);
-            BASE.Logger.debug("-------CREATE WDT STOCK IN RES-------{}", createWdtStockInRes)
-            const payloadRes = CORE.JSON.parse(createWdtStockInRes.payload)
-            if (payloadRes.code == 0) {
-                // 更新创建采购入库单号
-                storage.wdtDocNum = storage.wdtDocNum + ';' + payloadRes.stockin_no
-                storage.syncStatus = null
-                storage.syncMsg = null
-                H0.ModelerHelper.updateByPrimaryKey(storageModeler, tenantId, storage, true)
-            } else {
-                if(payloadRes.code == 2470) {
-                    // 采购入库单已经存在, 查询采购入库单, 并更新信息
-                    const queryPOStockInParam = {
-                        stockin_outer_no: storage.docNum
-                    }
-                    BASE.Logger.debug('-------queryPOStockInParam-------{}', queryPOStockInParam)
-                    let queryPOStockInRes = BASE.FeignClient.selectClient(secondServiceId)
-                        .doPost(buildParamsPath, queryPOStockInParam);
-                    BASE.Logger.debug('-------queryPOStockInRes-------{}', queryPOStockInRes)
-                    queryPOStockInRes = CORE.JSON.parse(queryPOStockInRes)
-                    if (createStockInParamRes.sign != null) {
-                        const queryParam = {
-                            bodyParamMap: queryPOStockInRes
-                        }
-                        let queryStockInRes = BASE.FeignClient.selectClient(serverId)
-                            .doPost(queryPoStockInPath, queryParam);
-                        queryStockInRes = CORE.JSON.parse(queryStockInRes);
-                        BASE.Logger.debug("-------queryStockInRes-------{}", queryStockInRes)
-                        const poStockInPayloadRes = CORE.JSON.parse(queryStockInRes.payload)
-                        if (poStockInPayloadRes.code == 0) {
-                            const stockIn = poStockInPayloadRes.stockin_list[0];
-                            storage.wdtDocNum = storage.wdtDocNum + ';' + stockIn.stockin_no
-                            storage.syncStatus = null
-                            storage.syncMsg = null
-                            H0.ModelerHelper.updateByPrimaryKey(storageModeler, tenantId, storage, true)
-                        }
-                    }
-                } else {
-                    storage.syncStatus = '失败'
-                    storage.syncMsg = payloadRes.message
-                    storage.docStatusCode = 'PUSH_FAIL'
-                    H0.ModelerHelper.updateByPrimaryKey(storageModeler, tenantId, storage, true)
-                    return payloadRes.message
+        //let createStockInParamRes = BASE.FeignClient.selectClient(secondServiceId)
+        //    .doPost(buildParamsPath, createStockInParam);
+        //BASE.Logger.debug('-------createStockInParamRes-------{}', createStockInParamRes)
+        //createStockInParamRes = CORE.JSON.parse(createStockInParamRes)
+        //if (createStockInParamRes.sign != null) {
+        //    const param = {
+        //        bodyParamMap: createStockInParamRes
+        //    }
+        // 调用旺店通创建采购入库单接口
+        //let createWdtStockInRes = BASE.FeignClient.selectClient(serverId)
+        //    .doPost(createPoStockInPath, param);
+        let createWdtStockInRes = BASE.FeignClient.selectClient(secondServiceId)
+            .doPost(createPoStockInPath, createStockInParam);
+        createWdtStockInRes = CORE.JSON.parse(createWdtStockInRes);
+        BASE.Logger.debug("-------CREATE WDT STOCK IN RES-------{}", createWdtStockInRes)
+        const payloadRes = CORE.JSON.parse(createWdtStockInRes.message)
+        if (payloadRes.code == 0) {
+            // 更新创建采购入库单号
+            storage.wdtDocNum = storage.wdtDocNum + ';' + payloadRes.stockin_no
+            storage.syncStatus = null
+            storage.syncMsg = null
+            H0.ModelerHelper.updateByPrimaryKey(storageModeler, tenantId, storage, true)
+        } else {
+            if (payloadRes.code == 2470) {
+                // 采购入库单已经存在, 查询采购入库单, 并更新信息
+                const queryPOStockInParam = {
+                    stockin_outer_no: storage.docNum
                 }
+                BASE.Logger.debug('-------queryPOStockInParam-------{}', queryPOStockInParam)
+                let queryPOStockInRes = BASE.FeignClient.selectClient(secondServiceId)
+                    .doPost(buildParamsPath, queryPOStockInParam);
+                BASE.Logger.debug('-------queryPOStockInRes-------{}', queryPOStockInRes)
+                queryPOStockInRes = CORE.JSON.parse(queryPOStockInRes)
+                if (createStockInParamRes.sign != null) {
+                    const queryParam = {
+                        bodyParamMap: queryPOStockInRes
+                    }
+                    let queryStockInRes = BASE.FeignClient.selectClient(serverId)
+                        .doPost(queryPoStockInPath, queryParam);
+                    queryStockInRes = CORE.JSON.parse(queryStockInRes);
+                    BASE.Logger.debug("-------queryStockInRes-------{}", queryStockInRes)
+                    const poStockInPayloadRes = CORE.JSON.parse(queryStockInRes.payload)
+                    if (poStockInPayloadRes.code == 0) {
+                        const stockIn = poStockInPayloadRes.stockin_list[0];
+                        storage.wdtDocNum = storage.wdtDocNum + ';' + stockIn.stockin_no
+                        storage.syncStatus = null
+                        storage.syncMsg = null
+                        H0.ModelerHelper.updateByPrimaryKey(storageModeler, tenantId, storage, true)
+                    }
+                }
+            } else {
+                storage.syncStatus = '失败'
+                storage.syncMsg = payloadRes.message
+                storage.docStatusCode = 'PUSH_FAIL'
+                H0.ModelerHelper.updateByPrimaryKey(storageModeler, tenantId, storage, true)
+                return payloadRes.message
             }
         }
+        //}
     }
     // 调取标准接口【直接转移】生成库存事务
     storage = H0.ModelerHelper.selectOne(storageModeler, tenantId, {
