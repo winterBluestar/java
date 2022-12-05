f95ed3cc888df7cc92bc01b6ae830d484cb8d050
chenxudong <xudong.chen@zone-cloud.com>
2022-12-05 15:44:01 +0800

    [fix]

--- model-project/demo-server/src/main/resources/web/ximenzi/zcus.shxmz.process.start.query.js
+++ model-project/demo-server/src/main/resources/web/ximenzi/zcus.shxmz.process.start.query.js
@@ -1,10 +1,10 @@
 function process(input) {
     BASE.Logger.debug('-------input-------{}', input)
     const wipServiceId = "zosc-wip";
-    const tenantId = CORE.CurrentContext.getTenantId();
-    //const tenantId = 96;
+    //const tenantId = CORE.CurrentContext.getTenantId();
+    const tenantId = 95;
     const customerModeler = 'zopo_customer';
-    let sql = "select zpot.customer_mo_num,zpot.creation_date,zpoto.sequence_num,zpoto.owner_operation_id,zpoto.operation_code from zwip_process_oem_task zpot left join zwip_process_oem_task_operation zpoto on zpot.process_oem_task_id = zpoto.process_oem_task_id where zpot.tenant_id = #{tenantId}"
+    let sql = "select zpot.customer_mo_num,zpot.creation_date,zpoto.sequence_num,zpoto.owner_operation_id,zpoto.operation_code from zwip_process_oem_task zpot left join zwip_process_oem_task_operation zpoto on zpot.process_oem_task_id = zpoto.process_oem_task_id where "
     const queryParamMap = {
         tenantId: tenantId
     };
@@ -27,29 +27,32 @@ function process(input) {
                 }
             }
         }
-        sql = sql + " and zpot.creation_date &gt;= '" + queryParamMap.startTime + "'"
+        sql = sql + " zpot.creation_date &gt;= '" + queryParamMap.startTime + "'"
         sql = sql + " and zpot.creation_date &lt;= '" + queryParamMap.endTime + "'"
     }
     // 查询客户id
     let customerCodeRes = H0.LovHelper.queryLovValue('ZCUS.SHXMZ.PROCESS.START.CUSTOMER', tenantId, 'zh_CN');
     if (customerCodeRes != null && customerCodeRes.length > 0) {
         let customerIdStr = ''
+        let supplierTenantIdStr = ''
         for (let customerCodeIndex = 0; customerCodeIndex < customerCodeRes.length; customerCodeIndex++) {
             const customerCode = customerCodeRes[customerCodeIndex]
-            let customer = H0.ModelerHelper.selectOne(customerModeler, tenantId, {
-                "customerCode": customerCode.value
+            let customer = H0.ModelerHelper.selectOne(customerModeler, customerCode.meaning, {
+                "customerCode": '30S1'
             });
             if (customer != null) {
                 if (customerCodeIndex != customerCodeRes.length - 1) {
                     customerIdStr = customerIdStr + customer.customerId + ','
+                    supplierTenantIdStr = supplierTenantIdStr + customerCode.meaning + ','
                 } else {
                     customerIdStr = customerIdStr + customer.customerId
+                    supplierTenantIdStr = supplierTenantIdStr + customerCode.meaning
                 }
             }
         }
         if (customerIdStr != null) {
-            queryParamMap.customerIdStr = '(' + customerIdStr + ')'
-            sql = sql + " and zpot.customer_id in " + queryParamMap.customerIdStr
+            sql = sql + " and zpot.customer_id in " + '(' + customerIdStr + ')'
+            sql = sql + " and zpot.tenant_id in " + '(' + supplierTenantIdStr + ')'
         }
     } else {
         BASE.Logger.debug('-------------------查询工序开始无客户信息则不执行后续逻辑-------------------')
@@ -65,7 +68,7 @@ function process(input) {
             const oem = oemRes[oemIndex]
             oem.fileName = oem.operationCode + 'erp' + oem.customerMoNum + oem.sequenceNum + '.xml'
             //oem.charsetName = 'utf-8'
-            oem.directory = '/data/xmz-dev'
+            oem.directory = '/data/xmz-dev/fab'
         }
     }
     BASE.Logger.debug('-------查询工序开始返回结果-------{}', oemRes)
