103654f458d362cf5ae1f7ef0e87f0101a94cec4
chenxudong <xudong.chen@zone-cloud.com>
2022-12-06 16:54:08 +0800

    [fix]

---
+++ model-project/demo-server/src/main/resources/web/ximenzi/zcus.shxmz.process.stop.report.query.js
@@ -0,0 +1,67 @@
+function process(input) {
+    BASE.Logger.debug('-------上海西门子工序终止上报: input-------{}', input)
+    //const tenantId = CORE.CurrentContext.getTenantId();
+    const tenantId = 96;
+    const secondServiceId = "zosc-second-service";
+    const wipServiceId = "zosc-wip";
+    let taskExpandSql = "select tenant_id, id, process_oem_task_id from zcus_shxmz_process_oem_task_expand where tenant_id = #{tenantId} "
+    let taskSql = "select zpot.process_oem_task_id,zpot.customer_mo_num,zpot.creation_date,zpoto.id,zpoto.operation_code,zpoto.sequence_num from zwip_process_oem_task zpot left join zwip_process_oem_task_operation zpoto on zpot.process_oem_task_id = zpoto.process_oem_task_id where "
+    const queryParamMap = {
+        tenantId: tenantId
+    };
+    if (input != null && input.expandIdListStr != null) {
+        taskExpandSql = taskExpandSql + "and id in (" + input.expandIdListStr + ") order by CREATION_DATE desc limit 2";
+    } else {
+        taskExpandSql = taskExpandSql + "and JUDGMENT_EXP_FLAG = 0 and judgment_result is not null order by CREATION_DATE desc limit 2"
+    }
+    // 查询task-expand数据
+    BASE.Logger.debug('-------查询工序终止上报task-expand-SQL参数-------{}', queryParamMap)
+    let taskExpandRes = H0.SqlHelper.selectList(secondServiceId, taskExpandSql, queryParamMap);
+    BASE.Logger.debug('-------查询工序终止上报task-expand-SQL参数-------{}', taskExpandRes)
+    let taskIdStr = ''
+    const expandMap = new Map()
+    if (taskExpandRes != null && taskExpandRes.length > 0) {
+        for (let expandIndex = 0; expandIndex < taskExpandRes.length; expandIndex++) {
+            const taskExpand = taskExpandRes[expandIndex]
+            if (expandIndex !== (taskExpandRes.length - 1)) {
+                taskIdStr = taskIdStr + taskExpand.processOemTaskId + ','
+            } else {
+                taskIdStr = taskIdStr + taskExpand.processOemTaskId
+            }
+            expandMap.set(taskExpand.processOemTaskId, taskExpand.id)
+        }
+    }
+    // 查询task数据
+    if (taskIdStr != null && taskIdStr !== '') {
+        const concatStr = "zpot.process_oem_task_id in (" + taskIdStr + ") order by zpot.creation_date desc limit 2"
+        taskSql = taskSql + concatStr
+        BASE.Logger.debug('-------查询工序终止上报task-SQL-------{}', taskSql)
+        let qualityRes = H0.SqlHelper.selectList(wipServiceId, taskSql, {});
+        BASE.Logger.debug('-------查询工序终止上报task-SQL返回参数-------{}', taskExpandRes)
+        if (qualityRes != null && qualityRes.length > 0) {
+            for (let qualityIndex = 0; qualityIndex < qualityRes.length; qualityIndex++) {
+                const quality = qualityRes[qualityIndex]
+                quality.expandId = expandMap.get(quality.processOemTaskId)
+                quality.fileName = quality.operationCode + 'erp' + quality.customerMoNum + quality.sequenceNum + '.xml'
+                quality.directory = '/data/xmz-dev/fab'
+                quality.contextStr = '<?xml version="1.0" encoding="Windows-1252"?>' +
+                    '<FABImport Version="1.0">' +
+                    '<FeedbackWorkplaceToFab>' +
+                    '<OrderStoppedFeedbacks>' +
+                    '<OrderStopped OrderNo="' + quality.customerMoNum + '">' +
+                    '<OrderStatus>0</OrderStatus>' +
+                    '<FinishedParts>0</FinishedParts>' +
+                    '<ScrapParts>0</ScrapParts>' +
+                    '<ProcessingTime>0</ProcessingTime>' +
+                    '<Timestamp>' + quality.creationDate +
+                    '</TimeStamp>' +
+                    '</OrderStopped>' +
+                    '</OrderStoppedFeedbacks>' +
+                    '</FeedbackWorkplaceToFab>' +
+                    '</FABImport>'
+            }
+        }
+        BASE.Logger.debug('-------查询工序终止上报返回结果-------{}', qualityRes)
+        return qualityRes
+    }
+}
