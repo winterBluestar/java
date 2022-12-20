45638f290503edb924e1235e1e563402444f2442
chenxudong <xudong.chen@zone-cloud.com>
2022-12-20 15:21:39 +0800

    [fix]

--- model-project/demo-server/src/main/resources/web/ximenzi/zcus.shxmz.wip.oem.pending.export.js
+++ model-project/demo-server/src/main/resources/web/ximenzi/zcus.shxmz.wip.oem.pending.export.js
@@ -8,7 +8,34 @@ function process(input) {
     const queryPath = '/v1/' + tenantId + '/moOperation-oem/getPendingOemOperation?customizeUnitCode=ZOSC.WIP.OPERATIONOEMPLATFORM.LIST.PENDING.TABLE&page=0&size=-1';
 
     // 查询数据
-    const param = input
+    const param = {tenantId: tenantId}
+    if (input.customizeUnitCode != null) {
+        param.customizeUnitCode = input.customizeUnitCode[0]
+    }
+    if (input.organizationId != null) {
+        param.organizationId = input.organizationId[0]
+    }
+    if (input.taskCode != null) {
+        param.taskCode = input.taskCode[0]
+    }
+    if (input.companyId != null) {
+        param.companyId = input.companyId[0]
+    }
+    if (input.itemId != null) {
+        param.itemId = input.itemId[0]
+    }
+    if (input.itemSkuId != null) {
+        param.itemSkuId = input.itemSkuId[0]
+    }
+    if (input.skuEnabledFlag != null) {
+        param.skuEnabledFlag = input.skuEnabledFlag[0]
+    }
+    if (input.customerId != null) {
+        param.customerId = input.customerId[0]
+    }
+    if (input.customerName != null) {
+        param.customerName = input.customerName[0]
+    }
     BASE.Logger.debug('-------------------工序代工平台待处理查询参数转换-------------------{}', param)
     let queryRes = H0.FeignClient.selectClient(wipServerId).doPost(queryPath, param)
     queryRes = CORE.JSON.parse(queryRes)
